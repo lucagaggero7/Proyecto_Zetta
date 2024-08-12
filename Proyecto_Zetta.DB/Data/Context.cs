0828981009c5dfb7b73c615378fa537f14b9d970
Stefano Gaggero <stefanolucagaggero@gmail.com>
2024-08-12 04:11:52 -0300

    Agregar archivos de proyecto.

---
+++ Proyecto_Zetta.DB/Data/Context.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Proyecto_Zetta.DB.Data.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Zetta.DB.Data
+{
+    public class Context : DbContext
+    {
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Obra> Obras { get; set; }
+        public DbSet<Instalador> Instaladores { get; set; }
+        public DbSet<Presupuesto> Presupuestos { get; set; }
+
+        public Context(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            var cascadeFKs = modelBuilder.Model.GetEntityTypes()
+                                          .SelectMany(t => t.GetForeignKeys())
+                                          .Where(fk => !fk.IsOwnership
+                                          && fk.DeleteBehavior == DeleteBehavior.Cascade);
+
+            foreach (var fk in cascadeFKs)
+            {
+                fk.DeleteBehavior = DeleteBehavior.Restrict;
+            }
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Proyecto_Zetta.DB/Data/Entity/Obra.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Zetta.DB.Data.Entity
+{
+    public class Obra : EntityBase
+    {
+        [Required(ErrorMessage = "El estado es obligatorio.")]
+        [MaxLength(12, ErrorMessage = "Maximo numero de caracteres {1}.")]
+        public string Estado { get; set; }
+
+        [Required(ErrorMessage = "El tipo de obra es obligatorio.")]
+        [MaxLength(40, ErrorMessage = "Maximo numero de caracteres {1}.")]
+        public string Tipo { get; set; }
+
+        [MaxLength(100, ErrorMessage = "Maximo numero de caracteres {1}.")]
+        public string? Descripcion { get; set; }
+
+        [Required(ErrorMessage = "La opcion materiales es obligatoria.")]
+        [MaxLength(2, ErrorMessage = "Maximo numero de caracteres {1}.")]
+        public char IncluyeMateriales { get; set; }
+
+        [Required(ErrorMessage = "La fecha de alta es obligatoria.")]
+        public DateTime FechaAlta { get; set; }
+        public DateTime? FechaBaja { get; set; }
+
+        [Required(ErrorMessage = "La opcion mantenimiento es obligatoria.")]
+        [MaxLength(2, ErrorMessage = "Maximo numero de caracteres {1}.")]
+        public char Mantenimiento { get; set; }
+        public DateTime? FechaMantenimiento{ get; set; }
+
+        //claves foraneas 
+        public int InstaladorId { get; set; }
+        public Instalador Instalador { get; set; }
+        public int ClienteId { get; set; }
+        public Cliente Cliente { get; set; }
+    }
+}
