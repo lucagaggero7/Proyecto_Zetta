c8cea5739a3d179e0d8b014f0095a3538c2da2fc
Stefano Gaggero <stefanolucagaggero@gmail.com>
2024-09-22 20:48:26 -0300

    Nuevo ContratoControllers para el abmObra.

---
+++ Proyecto_Zetta.DB/Migrations/20240922104541_ErrorFKey.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Proyecto_Zetta.DB.Migrations
+{
+    /// <inheritdoc />
+    public partial class ErrorFKey : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Obras_Instaladores_InstaladorId",
+                table: "Obras");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Obras_InstaladorId",
+                table: "Obras");
+
+            migrationBuilder.DropColumn(
+                name: "InstaladorId",
+                table: "Obras");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "InstaladorId",
+                table: "Obras",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Obras_InstaladorId",
+                table: "Obras",
+                column: "InstaladorId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Obras_Instaladores_InstaladorId",
+                table: "Obras",
+                column: "InstaladorId",
+                principalTable: "Instaladores",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
