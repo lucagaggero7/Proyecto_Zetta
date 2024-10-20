5b36c21448af91beb51fcbd73bd0a0b919d3c173
Stefano Gaggero <stefanolucagaggero@gmail.com>
2024-10-20 03:01:23 -0300

    Agregado de proyecto client ya funcionando front de blazor webassembly por defecto

--- Proyecto_Zetta.Server/Program.cs
+++ Proyecto_Zetta.Server/Program.cs
@@ -1,18 +1,24 @@
 using Microsoft.EntityFrameworkCore;
 using Proyecto_Zetta.DB.Data;
 using Proyecto_Zetta.Server.Repositorio;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions(
+x => x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 
 builder.Services.AddDbContext<Context>(op => op.UseSqlServer("name=conn"));
+
 
 
 builder.Services.AddAutoMapper(typeof(Program));
@@ -32,8 +38,15 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseBlazorFrameworkFiles();
+app.UseStaticFiles();
+app.UseRouting();
+app.MapRazorPages();
+
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.MapFallbackToFile("index.html");
 
 app.Run();
