55755168a696e28692e758ae8058e7ed9742a7ca
Stefano Gaggero <stefanolucagaggero@gmail.com>
2024-11-18 17:19:17 -0300

    swagger solo en development y nuevos estilos al navbar

---
+++ Proyecto_Zetta.Server/Controllers/EntornoControllers.cs
@@ -0,0 +1,25 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+namespace Proyecto_Zetta.Server.Controllers
+{
+    [Route("api/configuracion")]
+    [ApiController]
+    public class ConfiguracionController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public ConfiguracionController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet("entorno")]
+        public ActionResult<string> ObtenerEntorno()
+        {
+            var entorno = _configuration["ASPNETCORE_ENVIRONMENT"];
+            return Ok(entorno);  // Devuelve el valor como un string
+        }
+    }
+
+}
